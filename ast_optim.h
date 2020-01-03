763ff156fa90d2d2f18d4628fbb4ae047c0462ac
begriche <massinissa.begriche@etu.unistra.fr>
2020-01-03 16:38:56 +0100

    ast_optim

---
+++ ast_optim.h
@@ -0,0 +1,12 @@
+#ifndef __AST_OPTIM_H__
+#define __AST_OPTIM_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ast.h"
+
+ast* ast_divide(ast*);
+void ast_set(ast*, ast*);
+
+#endif
