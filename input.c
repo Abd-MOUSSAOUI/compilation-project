59f66de18ca1a68cda8d58245c7d7f43e163ee28
Putchii <putchii@MacBook-Air-de-Putchii.local>
2020-01-11 11:03:35 +0100

    update readme

--- input.c
+++ input.c
@@ -1,42 +1,26 @@
-int func1(int a, int b) {
-  int c = 1;
-  return c;
-}
-
-int func2(int a, int b) {
-  int c = 1;
-  return c;
-}
-
-int func3(int a, int b) {
-  int c = 1;
-  return c;
-}
-
-int func4(int a, int b) {
-  int c = 1;
-  return c;
-}
-
-int func5(int a, int b) {
-  int c = 1;
-  return c;
-}
-
-int func6(int a, int b) {
-  int c = 1;
-  return c;
-}
-
-int func7(int a, int b) {
-  int c = 1;
-  return c;
-}
-
-int main(){
-  int i = 10;
-  int tab[1][2][i];
-  int tab1[2] = {1,2};
-  tab[1][2][2] = tab1[0];
-  tab[2][2][0] = tab;
-}
+  int func()
+  {
+    int i;
+    int lo;
+    int hi; 
+    int v[10];
+    int val;  
+    for (i = lo; i < hi; i++) {
+      v[i] = val;
+    }
+  }
+ 
+  int func2(int a, int b)
+  {
+    int n = 16;
+    int i;
+    int ty[n]; 
+    int tx[n];
+    for (i = 0; i < n/2; i++) {
+      tx[i] = 1;
+    }
+  }
+  int main()
+  {
+    return 0;
+  }
